c18ef920bf5a7bfa3a18557b1637c84a7b87973a
Caio Nascimento <caionsantos2000@gmail.com>
2021-11-16 22:56:10 -0300

    feat: add irock emotions

--- 3 - irock2/irock.js
+++ 3 - irock2/irock.js
@@ -1,11 +1,44 @@
-const rock = document.getElementById("rock")
-const message = () => alert('Hello, I am your pet rock.')
+const rock = document.getElementById("rock");
+const body = document.querySelector("body");
+const check = document.getElementById("check");
+// parei na linha 97
 
+const greetUser = () => alert("Hello, I am your pet rock.");
+
+const sadRock = () => {
+  rock.src = "rock_sad.png";
+};
+
+const furiousRock = () => {
+  setInterval(() => {
+    if (Math.floor(Math.random() * 1000) <= 50) {
+      rock.src = "rock_furious.png";
+      console.log("furiosa");
+    }
+  }, 10 * 1000);
+};
+
+const aloneRock = () => {
+  setTimeout(() => {
+    console.log("chamou o intervalo do alone");
+    if (rock.src != "rock_furious.png") {
+      rock.src = "rock.png";
+    }
+  }, 10 * 1000);
+};
+
+let userName;
 const touchRock = () => {
-  let userName = prompt("What is your name?","Enter your name here.")
-
-  if(userName != "Enter your name here." && userName != null){
-    rock.src = "rock_happy.png"
-    alert(`It is good to meet you, ${userName}`)
+  if (userName == undefined || userName == "Enter your name here.") {
+    userName = prompt("What is your name?", "Enter your name here.");
+    if(userName != "Enter your name here."){
+      rock.src = "rock_happy.png";
+      alert(`It is good to meet you, ${userName}`);
+      aloneRock()
+    }
+  }else{
+    alert(`Obrigado ${userName} eu gosto de atenção`)
+    rock.src = "rock_happy.png";
+    aloneRock()
   }
-}
+};
