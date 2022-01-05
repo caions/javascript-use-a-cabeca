3a7bef3fc6d5da6cb65e64a38deb14f0f6cebfb2
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-01-04 23:06:13 -0300

    feat:  writecookie function

--- 3 - irock2/irock.js
+++ 3 - irock2/irock.js
@@ -1,7 +1,7 @@
 const rock = document.getElementById("rock");
 const body = document.querySelector("body");
 const check = document.getElementById("check");
-// parei na linha 97
+// parei na linha 107
 
 const greetUser = () => alert("Hello, I am your pet rock.");
 
@@ -29,18 +29,21 @@ const furiousRock = () => {
   setInterval(() => {
     if (Math.floor(Math.random() * 1000) <= 50) {
       rock.src = "rock_furious.png";
-      console.log("furiosa");
+      alert("Estou com raiva sem nenhum motivo");
     }
   }, 10 * 1000);
 };
 
 const aloneRock = () => {
   setTimeout(() => {
-    console.log("chamou o intervalo do alone");
     if (rock.src != "rock_furious.png") {
       rock.src = "rock.png";
     }
   }, 10 * 1000);
+};
+
+const writeCookie = (name) => {
+  document.cookie = `username=${name}`;
 };
 
 let userName;
@@ -51,10 +54,11 @@ const touchRock = () => {
     if (userName != "Enter your name here.") {
       rock.src = "rock_happy.png";
       alert(`It is good to meet you, ${userName}`);
+      writeCookie(userName);
       aloneRock();
     }
   } else {
-    alert(`Obrigado ${userName} eu gosto de atenção`);
+    alert(`Thank you ${userName} i like attention`);
     rock.src = "rock_happy.png";
     aloneRock();
   }
